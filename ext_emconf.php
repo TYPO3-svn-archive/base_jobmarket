51b3e48eb9a649194d392cf87084dda80ab8bf59
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-06-30 10:22:45 +0000

    1.3.3 - Initial check in
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/base_jobmarket/trunk@76945 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,50 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "base_jobmarket".
+#
+# Auto generated 17-03-2012 20:54
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Job Market - Template',
+	'description' => 'Basic Template for TYPO3 - especially for the extension Job Market (job_market)',
+	'category' => 'templates',
+	'shy' => 0,
+	'version' => '1.3.3',
+	'dependencies' => 'automaketemplate,browser',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Dirk Wildt - Die Netzmacher GbR',
+	'author_email' => 'http://wildt.at.die-netzmacher.de',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'automaketemplate' => '0.1.2-0.0.0',
+			'browser' => '3.9.9-0.0.0',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:22:{s:9:"ChangeLog";s:4:"19f2";s:11:"INSTALL.txt";s:4:"8ed9";s:12:"ext_icon.gif";s:4:"7159";s:14:"ext_tables.php";s:4:"4558";s:14:"doc/manual.pdf";s:4:"11b1";s:14:"doc/manual.sxw";s:4:"4418";s:26:"res/html/facebook_api.html";s:4:"5558";s:19:"res/html/index.html";s:4:"5a95";s:22:"res/html/css/basic.css";s:4:"974c";s:33:"res/html/css/background/typo3.gif";s:4:"0569";s:43:"res/html/css/background/wrap-horizontal.gif";s:4:"2f47";s:22:"res/images/favicon.ico";s:4:"4aa3";s:56:"res/images/01_rootpage/job_market_header_image_210px.jpg";s:4:"6acf";s:34:"res/pages/01_rootpage/tsConfig.txt";s:4:"a85d";s:52:"res/pages/01_rootpage/02_jobmarket_jobs/tsConfig.txt";s:4:"187f";s:47:"res/pages/01_rootpage/03_libraries/tsConfig.txt";s:4:"0d35";s:57:"res/pages/01_rootpage/03_libraries/04_header/tsConfig.txt";s:4:"187f";s:57:"res/pages/01_rootpage/03_libraries/05_footer/tsConfig.txt";s:4:"187f";s:46:"res/pages/01_rootpage/typoscript/constants.txt";s:4:"1294";s:42:"res/pages/01_rootpage/typoscript/setup.txt";s:4:"09b3";s:35:"static/base_jobmarket/constants.txt";s:4:"3a00";s:31:"static/base_jobmarket/setup.txt";s:4:"9668";}',
+	'suggests' => array(
+	),
+);
+
+?>
